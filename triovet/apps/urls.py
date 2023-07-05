090b47fedb54f81f45da4d248e8d6e08feded348
Nick Markovicz <hh.markovich.n@gmail.com>
2023-07-06 00:09:52 +0300

    Added URLs for appointments & for login/registration system

--- triovet/apps/urls.py
+++ triovet/apps/urls.py
@@ -15,17 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 
-from triovet.apps.schedule.views import appointments, confirm_appointment
+from triovet.apps.users.views import sign_up, sign_in, sign_out
+from triovet.apps.schedule.views import appointments, new_appointment, \
+    delete_appointment, approve_appointment
 from triovet.apps.core.views import index
 
 urlpatterns = [
     path("", index, name="index"),
     path("admin/", admin.site.urls),
+    path("new_appointment/", new_appointment, name="new_appointment"),
     path("appointments/", appointments, name="appointments"),
-    path("appointments/confirm", confirm_appointment, name="confirm_appointment")
+    path("appointments/delete/<int:appointment_id>/", delete_appointment, name="delete_appointment"),
+    path("appointments/approve/<int:appointment_id>/", approve_appointment, name="confirm_appointment"),
+    path("sign_up/", sign_up, name="sign_up"),
+    path("sign_in/", sign_in, name="sign_in"),
+    path("sign_out/", sign_out, name="sign_out")
 ]
 
 if settings.DEBUG:
