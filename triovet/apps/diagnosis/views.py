68830544315e5de76ce65de81f329e446f49f724
Nick Markovicz <hh.markovich.n@gmail.com>
2023-07-10 15:05:28 +0300

    Added diagnosis creation functionality & views

--- triovet/apps/diagnosis/views.py
+++ triovet/apps/diagnosis/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from triovet.apps.diagnosis.models import Diagnosis
+from triovet.apps.diagnosis.forms import NewDiagnosisForm
+
+
+def new_diagnosis(request):
+    if request.method == "POST":
+        form = NewDiagnosisForm(request.POST)
+        if form.is_valid():
+            title = request.POST.get("title")
+            note = request.POST.get("note")
+            Diagnosis.objects.create(schedule_id=request.GET.get("appointment_id"), title=title, note=note)
+            return render(request, "appointments.html")
+    else:
+        form = NewDiagnosisForm()
+        return render(request, "new_diagnosis.html", {"form": form})
