e53ecfb2940be291796f678104ac3437578cd3e7
Nick Markovicz <hh.markovich.n@gmail.com>
2023-07-10 15:07:35 +0300

    Added new links

--- triovet/apps/urls.py
+++ triovet/apps/urls.py
@@ -19,17 +19,24 @@ from django.urls import path
 from django.conf import settings
 
 from triovet.apps.users.views import sign_up, sign_in, sign_out
-from triovet.apps.schedule.views import appointments, new_appointment, \
-    delete_appointment, approve_appointment
+from triovet.apps.schedule.views import (appointments, new_appointment, delete_appointment, approve_appointment,
+                                         is_held_appointment,)
+from triovet.apps.patients.views import profile
+from triovet.apps.diagnosis.views import new_diagnosis
 from triovet.apps.core.views import index
+from triovet.apps.pets.views import new_pet
 
 urlpatterns = [
     path("", index, name="index"),
     path("admin/", admin.site.urls),
-    path("new_appointment/", new_appointment, name="new_appointment"),
     path("appointments/", appointments, name="appointments"),
+    path("appointments/new/", new_appointment, name="new_appointment"),
     path("appointments/delete/<int:appointment_id>/", delete_appointment, name="delete_appointment"),
     path("appointments/approve/<int:appointment_id>/", approve_appointment, name="confirm_appointment"),
+    path("appointments/is_held/<int:appointment_id>/", is_held_appointment, name="is_held_appointment"),
+    path("profile/", profile, name="profile"),
+    path("pet/new/", new_pet, name="new_pet"),
+    path("diagnosis/new", new_diagnosis, name="new_diagnosis"),
     path("sign_up/", sign_up, name="sign_up"),
     path("sign_in/", sign_in, name="sign_in"),
     path("sign_out/", sign_out, name="sign_out")
